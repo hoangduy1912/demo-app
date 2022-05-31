82f652af3cb9d9ce7a95acd1c741f488851f00eb
hoangduy1912 <nguyenhoangduy1912@gmail.com>
2022-05-31 21:14:36 +0700

    Update version 3

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask('hello-cloudbuild')
 
 @app.route('/')
 def hello():
-  return "Application Version 2\n"
+  return "Application Version 3\n"
 
 if __name__ == '__main__':
   app.run(host = '0.0.0.0', port = 8080)
--- test_app.py
+++ test_app.py
@@ -18,7 +18,7 @@ from app import hello
 class TestHelloApp(unittest.TestCase):
 
   def test_hello(self):
-    self.assertEqual(hello(), "Application Version 2\n")
+    self.assertEqual(hello(), "Application Version 3\n")
 
 if __name__ == '__main__':
   unittest.main()
