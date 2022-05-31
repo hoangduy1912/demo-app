1b5199cec78fe0a3f99145fb8e194c9d94c5f808
hoangduy1912 <nguyenhoangduy1912@gmail.com>
2022-05-31 21:11:37 +0700

    Update version 2

--- test_app.py
+++ test_app.py
@@ -18,7 +18,7 @@ from app import hello
 class TestHelloApp(unittest.TestCase):
 
   def test_hello(self):
-    self.assertEqual(hello(), "Application Version 1\n")
+    self.assertEqual(hello(), "Application Version 2\n")
 
 if __name__ == '__main__':
   unittest.main()
